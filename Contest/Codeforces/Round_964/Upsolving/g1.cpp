10c1db6eb1f41c2b78e6dd3eb2bddc0c33fe7d4e
MissMti <142945466+MissMti@users.noreply.github.com>
2024-08-10 00:32:25 -0300

    Create g1.cpp

---
+++ Contest/Codeforces/Round_964/Upsolving/g1.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long MAX=1000000007;
+
+signed main() {
+    //ios::sync_with_stdio(0);
+    //cin.tie(NULL);cout.tie(NULL);
+
+    long long t; cin>>t;
+    for(long long i=0;i<t;i++){
+        long long min=1,max=999,l=333,r=666,rsp;
+        while(min<max-2){
+            cout<<"? "<<l<<" "<<r<<"\n"<<flush;
+            cin>>rsp;
+            if(rsp==l*r){
+               // cout<<"a";
+                min=r;
+                l=(2*min+max)/3;
+                r=(2*max+min)/3;
+            }
+            else{
+                if(rsp==l*(r+1)){
+                    //cout<<"b";
+                    max=r;
+                    min=l;
+                    l=(2*min+max)/3;
+                    r=(2*max+min)/3;
+                }
+                else{
+                    //cout<<"c";
+                    max=l;
+                    l=(2*min+max)/3;
+                    r=(2*max+min)/3;
+                }
+            }
+            //cout<<min<<" "<<max<<" "<<l<<" "<<r<<"\n";
+        }
+        if(max-min==2){
+            cout<<"? "<<min<<" "<<max<<"\n"<<flush;
+            int j;
+            cin>>j;
+            if(j==(min+1)*(max+1)){
+                max=min;
+            }
+            else{
+                min++;
+                cout<<"? "<<min<<" "<<max<<"\n"<<flush;
+                cin>>j;
+                if(j==(min+1)*(max+1)){
+                    max=min;
+                }
+            }
+        }
+        cout<<"! "<<max<<"\n";
+    }
+}
