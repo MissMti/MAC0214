79478fdf3267bc96bfee925dda21f3499da55dfc
MissMti <142945466+MissMti@users.noreply.github.com>
2024-08-10 00:22:46 -0300

    Create a.cpp

---
+++ Contest/Codeforces/Round_964/No_dia/a.cpp
@@ -0,0 +1,13 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    signed main() {
+        int t; cin >> t;
+     
+        for (int i = 0; i < t; i++) {
+            int a;
+            cin >> a;
+     
+            cout << a/10 + a%10 << "\n";
+        }
+    }
